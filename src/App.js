d77b3480dd6282cf3d28ac8490f9c7821f82f9a1
avikanodiya <avikanodiya745@gmail.com>
2021-02-16 10:32:55 +0530

    N

--- src/App.js
+++ src/App.js
@@ -5,26 +5,33 @@ import Home from "./Home";
 import Header from './Header';
 import Checkout from "./Checkout"
 import Login from "./Login"
-import {auth} from "./firebase"
-import {useStateValue} from "./StateProvider";
+import { auth } from "./firebase"
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./Orders"
+import Payment from "./Payment";
 
+const promise = loadStripe(
+  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+);
 
 function App() {
-  const [{},dispatch] = useStateValue();
+  const [{ }, dispatch] = useStateValue();
 
   useEffect(() => {
-    auth.onAuthStateChanged(authUser =>{
-      console.log("the user is >>>",authUser);
+    auth.onAuthStateChanged(authUser => {
+      console.log("the user is >>>", authUser);
 
       if (authUser) {
         //user logged in
 
         dispatch({
-          type:"SET_USER",
+          type: "SET_USER",
           user: authUser,
         });
 
-      } else{
+      } else {
         //user logged out
         dispatch({
           type: "SET_USER",
@@ -38,12 +45,22 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
